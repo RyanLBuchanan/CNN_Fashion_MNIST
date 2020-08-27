8f8ad5d949d0d87fe68ce1b3086d100aee422063
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 16:01:07 -0600

    python

---
+++ CNN_Fashion_MNIST.py
@@ -0,0 +1,30 @@
+# CNN Fashion MNIST from Advanced Computer Vision - Lazy Programmer
+# Input by Ryan L Buchanan 27AUG20
+
+from __future__ import print_function, division
+from builtins import range 
+# may need to update: pip install -U future
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, Input
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+# Helper
+def y2indicator(Y):
+    N = len(Y)
+    K = len(set(Y))
+    I = np.zeros((N, K))
+    I[np.arange(N), Y] = 1
+    return I
+
+
+# Get the data
+data = pd.read_csv('fashion-mnist_train.csv')
+data = data.as_matrix()
+    
+
+
